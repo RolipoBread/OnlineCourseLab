8668f0c7e3663eaf4a1f41dcb25449c157767419
RolipoBread <pawel.odintsow201@yandex.ru>
2026-02-25 00:09:14 +0300

    first commit

---
+++ src/main/java/com/example/onlinecourseslab/repository/CourseRepository.java
@@ -0,0 +1,9 @@
+package com.example.onlinecourseslab.repository;
+
+import com.example.onlinecourseslab.domain.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CourseRepository extends JpaRepository<Course, Long> {
+    List<Course> findByAuthor(String author); }
