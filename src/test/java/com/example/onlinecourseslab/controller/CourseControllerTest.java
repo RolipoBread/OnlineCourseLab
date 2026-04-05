1adf715c735631ee8d28f24c53c5712b091a5043
RolipoBread <pawel.odintsow201@yandex.ru>
2026-04-06 01:34:15 +0300

    add test-4

--- src/test/java/com/example/onlinecourseslab/controller/CourseControllerTest.java
+++ src/test/java/com/example/onlinecourseslab/controller/CourseControllerTest.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
